804db977182e11a9071af13d5fa872793ad85de4
Arunava Basu <arunavabasudev@gmail.com>
2024-02-01 21:20:21 +0530

    feat:update schema

--- src/db/schema/Models.ts
+++ src/db/schema/Models.ts
@@ -1,14 +1,15 @@
 import { InferSelectModel } from "drizzle-orm";
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const ModelsTable = pgTable("models", {
   id: uuid("id").primaryKey().notNull().defaultRandom(),
-  name: text("name").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  name: varchar("name", { length: 20 }).notNull(),
   logo: text("logo").notNull(),
-  description: text("description").notNull(),
+  link: text("link").notNull(),
+  description: varchar("description", { length: 400 }).notNull(),
   usage: text("usage").notNull(),
   applications: text("applications").notNull(),
-  createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
 
-export type models = InferSelectModel<typeof ModelsTable>;
+export type TModel = InferSelectModel<typeof ModelsTable>;
