e45616dcec4c8facd731a26a2ac15fd0c8e43bc3
Arunava Basu <arunavabasudev@gmail.com>
2024-02-02 20:02:36 +0530

    perf: improve api latency

--- src/app/api/models/route.ts
+++ src/app/api/models/route.ts
@@ -2,21 +2,41 @@ import { db } from "@/db/drizzle";
 import { ModelsTable } from "@/db/schema/Models";
 import { NextResponse } from "next/server";
 import { InferSelectModel, eq } from "drizzle-orm";
+import { loadEnvConfig } from "@next/env";
 
 export type models = InferSelectModel<typeof ModelsTable>;
+import { Redis } from "@upstash/redis";
 
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+
+const redis = new Redis({
+  url: "https://us1-beloved-louse-41082.upstash.io",
+  token: process.env.REDIS_URL || "",
+});
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id")?.trim();
-
+  const cacheKey = id ? `models:${id}` : "models:all";
   let models: any;
+  const cachedData = await redis.get(cacheKey);
+  if (cachedData) {
+    console.log("Cache Hit:", cacheKey);
+    return NextResponse.json(cachedData);
+  } else {
+    console.log("Cache Miss:", cacheKey);
+  }
+
   if (id) {
     models = await db.select().from(ModelsTable).where(eq(ModelsTable.id, id));
     if (models.length === 1) {
+      await redis.set(cacheKey, JSON.stringify(models[0]));
       return NextResponse.json(models[0]);
     }
   } else {
     models = await db.select().from(ModelsTable);
+    await redis.set(cacheKey, JSON.stringify(models));
   }
   return NextResponse.json(models);
 }
