8b45ee4927b3e3155ef3a53d98de3b25595cb42c
srinathshrestha <srinathshrestha9890@gmail.com>
2024-04-19 19:11:36 +0530

    changed the names

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="h-16 flex items-center justify-center bg-gray-800 text-white">
-      <p>© 2024 arunavabasu.com All rights reserved.</p>
+      <p>© 2024 ModelHuB.com All rights reserved.</p>
     </footer>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <header className="fixed top-0 left-0 right-0 h-16 flex items-center justify-between px-4 bg-white shadow-md">
         <Link className="flex items-center" href="/">
           {/* <MountainIcon className="h-6 w-6" /> */}
-          <span className="ml-2 text-lg font-semibold">HEX</span>
+          <span className="ml-2 text-lg font-semibold">ModelHuB</span>
         </Link>
       </header>
     </div>
